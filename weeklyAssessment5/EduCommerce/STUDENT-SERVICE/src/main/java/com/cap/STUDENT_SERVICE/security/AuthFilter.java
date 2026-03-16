2895d82e3965fd79e3c76433d72084a77f9b1d30
Yash <yash1402raj@gmail.com>
2026-03-16 12:29:52 +0530

    small ch

--- weeklyAssessment5/EduCommerce/STUDENT-SERVICE/src/main/java/com/cap/STUDENT_SERVICE/security/AuthFilter.java
+++ weeklyAssessment5/EduCommerce/STUDENT-SERVICE/src/main/java/com/cap/STUDENT_SERVICE/security/AuthFilter.java
@@ -4,12 +4,10 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.
-        UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.
-        WebAuthenticationDetailsSource;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -38,15 +36,19 @@ public class AuthFilter extends OncePerRequestFilter {
 
         String token = authHeader.substring(7);
 
-        String username = jwtUtil.extractUsername(token);
+        if(!jwtUtil.isTokenValid(token)){
+            filterChain.doFilter(request,response);
+            return;
+        }
 
-        if(username != null &&
-                SecurityContextHolder.getContext().getAuthentication() == null){
+        try {
+            String username = jwtUtil.extractUsername(token);
 
-            UserDetails userDetails =
-                    userDetailsService.loadUserByUsername(username);
+            if(username != null &&
+                    SecurityContextHolder.getContext().getAuthentication() == null){
 
-            if(jwtUtil.isTokenValid(token)){
+                UserDetails userDetails =
+                        userDetailsService.loadUserByUsername(username);
 
                 UsernamePasswordAuthenticationToken authToken =
                         new UsernamePasswordAuthenticationToken(
@@ -63,6 +65,10 @@ public class AuthFilter extends OncePerRequestFilter {
                         .getContext()
                         .setAuthentication(authToken);
             }
+        } catch (Exception e) {
+            logger.error("Token validation failed: ", e);
+            filterChain.doFilter(request,response);
+            return;
         }
 
         filterChain.doFilter(request,response);
